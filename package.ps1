33f9df10881357e6026ba1de120756c6d9538d4a
Keira Dueck <sylae@calref.net>
2024-07-07 12:01:39 -0700

    Update package.ps1

--- package.ps1
+++ package.ps1
@@ -1,3 +1,3 @@
 $7z = "C:\Program Files\7-Zip\7z"
-
+if (test-path static-loading-screen.op) { rm static-loading-screen.op -verbose }
 & $7z a -tzip static-loading-screen.op *.as info.toml default.png LICENSE
